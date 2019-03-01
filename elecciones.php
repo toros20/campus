f68bd8842e57aa3c3f3bd2998b20601754f61c5d
Marvin <marvinlink07@gmail.com>
2019-02-28 22:02:18 -0600

    cambios pequeños

--- elecciones.php
+++ elecciones.php
@@ -58,6 +58,7 @@
 
                     <div style="float:center"  align="center">
                         <form action="votosecundaria.php" method="get" align="center">
+                            <h3>Votaciones de Secundaria</h3>
                             <h3 align="center">Ingrese su c&oacute;digo de Votaci&oacute;n</h3>
                             <input style="width:250px;" required name="cuenta" id="cuenta" autofocus class="form-control" minlength = "6" maxlength="6">
                             <?php echo "<br />"; ?>
