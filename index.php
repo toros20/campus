2ff060a44140d79f9b0db5793a8cd7e27a9f5c8b
Marvin <marvinlink07@gmail.com>
2019-05-29 20:34:36 -0600

    30de mayo

--- index.php
+++ index.php
@@ -829,14 +829,14 @@
             <div class="modal-content">
               <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4 class="modal-title" id="myModalLabel">Horario de Salida para el 10 de Mayo</h4>
+                <h4 class="modal-title" id="myModalLabel">Comunicado para el 30 de Mayo</h4>
               </div>
               <div class="modal-body">
 
                 <!-- <a style="text-align:center;" href="pdf/Lista_utiles_secundaria.pdf" target="_blank">
                     <h5> Descargar Listados de utiles de Pre-Básica</h5> 
                 </a> -->
-                <img src="img/salida_10_de_mayo.png" alt="">
+                <img src="img/Comunicado.JPG" alt="">
                  
               </div>
 
